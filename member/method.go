47c8cec02b55e6f6a8a0cbfb6e638dc332c7eaf6
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-05-25 05:02:08 +0800

    update fine result

--- member/method.go
+++ member/method.go
@@ -1,10 +1,7 @@
 package member
 
 import (
-	"errors"
-	"github.com/open4go/log"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func (m *Model) GetByWxLoginID(id string) (*Model, error) {
@@ -14,17 +11,8 @@ func (m *Model) GetByWxLoginID(id string) (*Model, error) {
 	filter := bson.D{{Key: "identity.wx_login_id", Value: id}}
 
 	// 获取数据列表
-	cursor, err := coll.Find(m.Context.Context, filter)
-	if errors.Is(err, mongo.ErrNoDocuments) {
-		log.Log().WithField("id", id).Error(err)
-		return nil, err
-	}
+	err := coll.FindOne(m.Context.Context, filter).Decode(result)
 	if err != nil {
-		log.Log().WithField("id", id).Error(err)
-		return nil, err
-	}
-	if err = cursor.All(m.Context.Context, &result); err != nil {
-		log.Log().WithField("id", id).Error(err)
 		return nil, err
 	}
 	return result, nil
