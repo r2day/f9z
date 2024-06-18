e9bb548b0aa71fc5b1202dceba4b759ff4767e4a
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-18 23:29:02 +0800

    add get peroperties

--- product/method.go
+++ product/method.go
@@ -3,30 +3,33 @@ package product
 import (
 	"errors"
 	"github.com/open4go/log"
+	"github.com/r2day/f9z/property"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m *Model) GetByOrderID(id string) ([]*Model, error) {
+// GetProperty 获取属性配置
+func (m *Model) GetProperty() ([]*property.Model, error) {
 
-	results := make([]*Model, 0)
-	coll := m.Context.Handler.Collection(m.Context.Collection)
-	filter := bson.D{{Key: "identity.order_no", Value: id}}
+	productModel := property.Model{}
+	results := make([]*property.Model, 0)
+	coll := m.Context.Handler.Collection(productModel.CollectionName())
+	filter := bson.M{"_id": bson.M{"$in": m.Properties}}
 
 	// 获取数据列表
 	cursor, err := coll.Find(m.Context.Context, filter)
 	if errors.Is(err, mongo.ErrNoDocuments) {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
+		log.Log(m.Context.Context).WithField("m", m).Error(err)
 		return nil, err
 	}
 
 	if err != nil {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
+		log.Log(m.Context.Context).WithField("m", m).Error(err)
 		return nil, err
 	}
 
 	if err = cursor.All(m.Context.Context, &results); err != nil {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
+		log.Log(m.Context.Context).WithField("m", m).Error(err)
 		return nil, err
 	}
 	return results, nil
