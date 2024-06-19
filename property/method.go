43db7630b08a53c9d07cbbaca7372583baec97dc
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-20 02:27:11 +0800

    add price sum

--- property/method.go
+++ property/method.go
@@ -1,33 +1,17 @@
 package property
 
-import (
-	"errors"
-	"github.com/open4go/log"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-func (m *Model) GetByOrderID(id string) ([]*Model, error) {
-
-	results := make([]*Model, 0)
-	coll := m.Context.Handler.Collection(m.Context.Collection)
-	filter := bson.D{{Key: "identity.order_no", Value: id}}
-
-	// 获取数据列表
-	cursor, err := coll.Find(m.Context.Context, filter)
-	if errors.Is(err, mongo.ErrNoDocuments) {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
-		return nil, err
+// GetPriceSumByIds 获取属性配置总价格
+func (m *Model) GetPriceSumByIds(ids []int) (int64, error) {
+	idSet := make(map[int]struct{})
+	for _, id := range ids {
+		idSet[id] = struct{}{}
 	}
 
-	if err != nil {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
-		return nil, err
+	var totalPrice int64
+	for _, val := range m.Values {
+		if _, ok := idSet[val.Id]; ok {
+			totalPrice += val.Price
+		}
 	}
-
-	if err = cursor.All(m.Context.Context, &results); err != nil {
-		log.Log(m.Context.Context).WithField("id", id).Error(err)
-		return nil, err
-	}
-	return results, nil
+	return totalPrice, nil
 }
