9079f5943754d674b9acd11720090388b0516cca
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-25 14:59:30 +0800

    add sales counter

--- product/method.go
+++ product/method.go
@@ -34,3 +34,22 @@ func (m *Model) GetProperty() ([]*property.Model, error) {
 	}
 	return results, nil
 }
+
+// SalesCount 更新销量信息
+func (m *Model) SalesCount(salesIncrement int) error {
+	coll := m.Context.Handler.Collection(m.CollectionName())
+	// 创建过滤器和更新文档
+	filter := bson.M{"_id": m.ID}
+	update := bson.M{"$inc": bson.M{"sales": salesIncrement}}
+
+	// 更新产品销量信息
+	result, err := coll.UpdateOne(m.Context.Context, filter, update)
+	if err != nil {
+		log.Log(m.Context.Context).Error(err)
+		return err
+	}
+	log.Log(m.Context.Context).WithField("salesIncrement", salesIncrement).
+		WithField("id", m.ID.Hex()).WithField("result", result).
+		Debug("after sales count success")
+	return nil
+}
