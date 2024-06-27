039047b3de7286c1103f4dfea0694fc92381ab3a
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-28 03:51:14 +0800

    add address

--- address/method.go
+++ address/method.go
@@ -1,17 +1,1 @@
 package address
-
-// GetPriceSumByIds 获取属性配置总价格
-func (m *Model) GetPriceSumByIds(ids []int) (int64, error) {
-	idSet := make(map[int]struct{})
-	for _, id := range ids {
-		idSet[id] = struct{}{}
-	}
-
-	var totalPrice int64
-	for _, val := range m.Values {
-		if _, ok := idSet[val.Id]; ok {
-			totalPrice += val.Price
-		}
-	}
-	return totalPrice, nil
-}
